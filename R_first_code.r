71f7d95d4deaf9feda79bbd39aa603b9be47f661
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2021-10-20 16:03:48 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -5,9 +5,19 @@
 WATER <- c(100,200,300,400,500)
 WATER
 #marta data on fishes genomes
-fishes <- c(10,50,60,100,200)
+fishes <- c(10,50,60,100,200) 
 fishes
 
 # plot the diversity of fishes (y) versus the amount of water (x)
 plot(WATER, fishes)
+
+# the data we developed can be stored in a table
+# a table in R is called data frame
+
+streams <- data.frame(WATER, fishes)
+
+streams
+
+
+
 
