fbc8f6dbeff44d8506a61ce87a68cb38e66579c4
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2021-11-19 10:57:28 +0100

    Update R_code_Energy_rs.r

--- R_code_Energy_rs.r
+++ R_code_Energy_rs.r
@@ -1,3 +1,10 @@
 # R code for estimating energy in ecosystems
 
 library (raster)
+setwd("/Users/komaliftikhar/Desktop/lab/") # mac
+
+# importing the data
+l1992 <- brick("defor1.jpeg") #image
+l1992 
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+
