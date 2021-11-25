7cd0eadb4b234ad29981573c6a055ff7c9d8fbd1
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2021-11-25 20:34:17 +0100

    Update R_code_Energy_rs.r

--- R_code_Energy_rs.r
+++ R_code_Energy_rs.r
@@ -8,3 +8,18 @@ l1992 <- brick("defor1.jpeg") #image
 l1992 
 plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
 
+#starting again to work 
+library(raster)
+library(rgdal)
+setwd("/Users/komaliftikhar/Desktop/lab/") # mac
+
+l1992 <- brick("defor1.") #image
+l1992 
+
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+# day 2
+l2006 <- brick("defor2.jpg)
+l2006
+
+# plotting the reported image
+plotRGB(l2006, r=1, g=2, b=3, stretch="Lin")
