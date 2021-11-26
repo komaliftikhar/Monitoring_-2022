aa66d5c900d3573a7451e1d9b21ae0488214893d
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2021-11-26 10:17:40 +0100

    Update R_Code_chemcal_cycling.r

--- R_Code_chemcal_cycling.r
+++ R_Code_chemcal_cycling.r
@@ -6,3 +6,11 @@
 setwd("~/lab/en/") # Linux 
 # setwd("C:/lab/en/")  # windows
 # setwd("/Users/name/lab/en/") # mac
+library(raster)
+setwd("/Users/komaliftikhar/Desktop/lab/") # mac
+
+EN01 <- raster("EN_0001.png")
+EN01
+cl <- colorRampPalette(c('red','orange','yellow'))(100) 
+
+#PLOT NO2 values 
