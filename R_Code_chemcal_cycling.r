7df671163b2007f536489b2a65a798756b6f2d3c
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2021-11-26 09:46:49 +0100

    Update R_Code_chemcal_cycling.r

--- R_Code_chemcal_cycling.r
+++ R_Code_chemcal_cycling.r
@@ -1,1 +1,8 @@
+# R code for working chemical study
+# time series of NO2 change in Europe during the lockdown
+ 
 
+# Set the working directory
+setwd("~/lab/en/") # Linux 
+# setwd("C:/lab/en/")  # windows
+# setwd("/Users/name/lab/en/") # mac
