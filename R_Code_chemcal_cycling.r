687bba2ae9af9b37ebcde1ccd6c1709c3b138bbc
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2021-11-29 10:14:51 +0100

    Update R_Code_chemcal_cycling.r

--- R_Code_chemcal_cycling.r
+++ R_Code_chemcal_cycling.r
@@ -14,3 +14,24 @@ EN01
 cl <- colorRampPalette(c('red','orange','yellow'))(100) 
 
 #PLOT NO2 values 
+
+# day 2 importing all the data together.
+library(raster)
+setwd("/Users/komaliftikhar/Desktop/lab/EN")
+
+rlist <- list.files(pattern="EN")
+rlist
+
+list_rast <- lapply(rlist, raster)
+list_rast
+
+# now applying the stack function to import all the images all together
+EN_stack <- stack(list_rast)
+EN_stack
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100)
+
+#plot only single image now
+plot(EN_stack$EN_0001, col=cl)
+
+# difference
