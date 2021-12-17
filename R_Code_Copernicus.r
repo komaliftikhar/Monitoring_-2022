96e0417c492153d9235abd66e79ffe017820682c
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2021-12-17 10:40:26 +0100

    Update R_Code_Copernicus.r

--- R_Code_Copernicus.r
+++ R_Code_Copernicus.r
@@ -41,3 +41,59 @@ scale_fill_viridis()
 ggplot() +
 geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
 scale_fill_viridis(option="cividis") + ggtitle("cividis palette")
+
+Day 2
+
+ibrary(ncdf4)
+library(raster)
+library(RStoolbox)
+library(viridis)
+library(ggplot2)
+
+setwd("/Users/komaliftikhar/Desktop/lab/Copernicus/") #mac
+
+rlist <- list.files(pattern="SCE")
+rlist
+
+
+list_rast <- lapply(rlist, raster)
+list_rast
+
+
+snowstack <- stack(list_rast)
+snowstack
+
+snowstack <- stack(list_rast)
+snowstack
+
+ssummer <- snowstack$Snow.Cover.Extent.1
+ssummer
+swinter <- snowstack$Snow.Cover.Extent.2
+swinter
+
+#lets patchwork them together
+p1 <- ggplot() + 
+geom_raster(ssummer, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) +
+scale_fill_viridis(option="viridis") +
+ggtitle("Snow cover during my birthday!")
+ 
+
+
+p2 <- ggplot() + 
+
+geom_raster(swinter, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) +
+
+scale_fill_viridis(option="viridis") +
+
+ggtitle("Snow cover during freezing winter!") 
+
+p1 / p2
+
+
+# you can crop your image on a certain area
+
+# crop the stack to the extent of Sicily
+ext <- c(0, 20, 30, 50)
+ssummer_cropped <- crop(ssummer, ext)
+swinter_cropped <- crop(swinter, ext)
+
