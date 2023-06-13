15d2412c15eb55ed7589395732fa997648cb01cb
komaliftikhar <92572692+komaliftikhar@users.noreply.github.com>
2023-06-13 16:21:51 +0200

    Update Exam_Practice.r

--- Exam_Practice.r
+++ Exam_Practice.r
@@ -1,1 +1,21 @@
 # exam practice
+# here are the input data 
+
+#data of streams
+water <- c(100, 200, 300, 400, 500)
+
+# Marta dat aon fishes geneome
+fishes <- c(10, 50, 60, 100, 200)
+
+fishes
+
+water
+
+# to see the no of fishes or water no 
+
+#  now plot the diversity of fishes (y) versus the amount of water (x)
+
+plot(water, fishes)
+
+# after adding this function a graph is created.
+
